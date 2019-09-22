eedf14a449e0f70b838c60b117fbb3e98d7e19a6
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-09-22 14:18:51 +0200

    using custom initial state

--- src/index.js
+++ src/index.js
@@ -5,7 +5,17 @@ import App from './App';
 import eventBusFactory from './model/eventBus.js';
 import modelFactory from './model/state.js';
 
-const model = modelFactory();
+const loadState = () => {
+    const data = window.localStorage.getItem('STATE');
+    return data ? JSON.parse(data) : undefined;
+};
+
+const model = modelFactory(loadState());
 const eventBus = eventBusFactory(model);
+
+eventBus.subscribe(newState => {
+    window.localStorage.setItem('STATE', JSON.stringify(newState));
+});
+
 
 ReactDOM.render(<App eventBus={eventBus}/>, document.getElementById('root'));
