3bf7f86d6add74fa03dffa8a7b4afce7318d765a
francesco-strazzullo <francesco.strazzullo86@gmail.com>
2019-09-23 09:29:39 +0200

    using custom initial state

--- src/index.js
+++ src/index.js
@@ -7,16 +7,27 @@ import App from './App';
 import todosReducer from './model/todosReducer';
 import filterReducer from './model/filterReducer';
 
+const loadState = () => {
+    const data = window.localStorage.getItem('STATE');
+    return data ? JSON.parse(data) : undefined;
+};
+
+
 const reducers = combineReducers({
     todos: todosReducer,
     filter: filterReducer
 });
 
-const store = createStore(reducers);
+const store = createStore(reducers, loadState());
+
+store.subscribe(() => {
+    window.localStorage.setItem('STATE', JSON.stringify(store.getState()));
+});
+
 
 ReactDOM.render(
     <Provider store={store}>
         <App />
-    </Provider>, 
+    </Provider>,
     document.getElementById('root')
 );
